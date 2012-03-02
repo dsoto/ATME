08a739c621b52811f70bdea6de8b4ea90a5c9e43
Daniel Soto <drdrsoto@gmail.com>
2012-03-02 18:51:15 -0500

    places most recent result from ATME in a table and renders to webpage

--- twarduino.py
+++ twarduino.py
@@ -7,6 +7,7 @@ class Values:
     def __init__(self):
         self.value = 0
         self.th_list = None
+        self.ldict = {}
 
     def set_value(self, value):
         self.value = value
@@ -23,17 +24,70 @@ class Values:
     def get_list(self):
         return self.th_list
 
+    def set_dict(self, vals):
+        key = vals[2]
+        self.ldict[key] = vals
+
+    def get_dict(self):
+        return self.ldict
 
 class Webmin(resource.Resource):
     isLeaf = True
-
     def __init__(self):
         self.values = global_values
+        self.column_dict = {'uCTime':0,
+               'sequenceNumber':1,
+               'circuitID':2,
+               'isOn':3,
+               'VRMS':4,
+               'IRMS':5,
+               'VPEAK':6,
+               'IPEAK':7,
+               'PERIODUS':8,
+               'VA':9,
+               'W':10,
+               'VAEnergy':11,
+               'WEnergy':12,
+               'PFactor':13,
+               'VA_ACCUM':14,
+               'W_ACCUM':15,
+               'errorNum':16}
+        self.column_list = ['uCTime', 'sequenceNumber', 'circuitID', 'isOn',
+               'VRMS',
+               'IRMS',
+               'VPEAK',
+               'IPEAK',
+               'PERIODUS',
+               'VA',
+               'W',
+               'VAEnergy',
+               'WEnergy',
+               'PFactor',
+               'VA_ACCUM',
+               'W_ACCUM',
+               'errorNum']
+
+
 
     def render_GET(self, request):
         #ATME = self.service.ATME
-        return 'value = ' + str(self.values.get_value()) + '\n' + str(self.values.get_list()) + '\n'
-
+        #return 'value = ' + str(self.values.get_value()) + '\n' + str(self.values.get_list()) + '\n'
+        response = ''
+        thedict = self.values.get_dict()
+        response += '<table border="1">\n'
+        response += "<tr>\n"
+        for c in self.column_list:
+            response += "<td>" + str(c) + "</td>\n"
+        response += "</tr>\n"
+        keys = thedict.keys()
+        keys.sort()
+        for key in keys:
+            response += "<tr>\n"
+            for s in thedict[key]:
+                response += "<td>" + str(s) + "</td>\n"
+            response += '</tr>\n'
+        response += "</table>\n"
+        return response
 
 class Echo(basic.LineReceiver):
     def __init__(self):
@@ -45,13 +99,14 @@ class Echo(basic.LineReceiver):
         vals = line.split(',')
         if len(vals) > 2:
             if vals[2].isdigit():
-                print
-                print 'circuit #', vals[2]
-                print 'W', vals[10]
-                print 'VA', vals[9]
-                print vals
+                #print
+                #print 'circuit #', vals[2]
+                #print 'W', vals[10]
+                #print 'VA', vals[9]
+                #print vals
                 self.values.inc_value(int(vals[8]))
                 self.values.set_list(vals)
+                self.values.set_dict(vals)
         #acc = int(vals[0])
         #self.accumulator += acc
         #print self.accumulator
@@ -72,7 +127,23 @@ if __name__ == '__main__':
     reactor.run()
 
 
-
+column_dict = {'uCTime':0,
+               'sequenceNumber':1,
+               'circuitID':2,
+               'isOn':3,
+               'VRMS':4,
+               'IRMS':5,
+               'VPEAK':6,
+               'IPEAK':7,
+               'PERIODUS':8,
+               'VA':9,
+               'W':10,
+               'VAEnergy':11,
+               'WEnergy':12,
+               'PFactor':13,
+               'VA_ACCUM':14,
+               'W_ACCUM':15,
+               'errorNum':16}
 '''
 Order & Title          & Units     & Code Variable  & ADE Variable \\
 0     & uCTime         &           &                & ---          \\
